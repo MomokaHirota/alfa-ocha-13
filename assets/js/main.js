d5f7ecfe05333b5ddbd90f32907148ba76a1f6aa
MomokaHirota <89888430+MomokaHirota@users.noreply.github.com>
2021-09-10 10:59:57 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: '1940',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'c',
           // q2: 'えええ',
           // q3: 'おおお'
         },
